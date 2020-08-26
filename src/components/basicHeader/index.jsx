095a89f05b067cab135aa9f34aee6004bf09bbd4
xiaoquanquanxin <xxkl123@126.com>
2020-08-26 18:52:35 +0800

    新增更多页面

---
+++ src/components/basicHeader/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.less';
+
+
+//  脚部信息
+export function BasicHeader({ xx }){
+    return (
+        <header className='basic-header border-grey-blue'>
+            我是头部
+        </header>
+    );
+}
---
+++ src/pages/index/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import { BasicHeader } from '../../components/basicHeader';
+
+function App(){
+    return (
+        <div className="App">
+            <BasicHeader/>
+            我是首页
+        </div>
+    );
+}
+
+export default App;
