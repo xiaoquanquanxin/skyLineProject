cddcd97f03160e0b90f64f40299de6493607065b
xiaoquanquanxin <xxkl123@126.com>
2020-08-31 19:00:27 +0800

    了解更多

---
+++ src/components/index/learnMore/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './index.less';
+
+export const LearnMore = () => {
+    return (
+        <div className={style.learnMore}>
+            <img src={require('@media/index/HomeAI.gif')}
+                 className={style.homeAI}/>
+            <div className={style.learnMoreContent}>
+                <h1 className={style.learnMoreTitle}>旭日初升，踏上征程</h1>
+                <a href='' className={style.learnMoreLink}>了解更多</a>
+            </div>
+        </div>
+    );
+};
