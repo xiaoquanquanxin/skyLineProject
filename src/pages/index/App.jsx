ed940755b601c6fb960e8352fd00fcd6eb5faa6a
xiaoquanquanxin <xxkl123@126.com>
2020-08-31 14:58:30 +0800

    首页加芯片

--- src/pages/index/App.jsx
+++ src/pages/index/App.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './index.css';
 import { BasicHeader } from '@components/basicHeader';
-import BasicBanner from '@components/basicBanner';
-
+import { BasicBanner } from '@components/basicBanner';
+import { HomeChip } from '@components/index/homeChip';
 
 function App(){
     return (
         <div className="App">
+            {/*头部*/}
             <BasicHeader/>
+            {/*主横幅*/}
             <BasicBanner/>
-
+            {/*首页芯片*/}
+            <HomeChip/>
         </div>
     );
 }
-
-
 
 export default App;
 
