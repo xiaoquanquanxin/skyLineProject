a881da2712311dbcb16aa7634c9c1242965c7515
xiaoquanquanxin <xxkl123@126.com>
2020-09-03 14:43:58 +0800

    rem

--- src/utils/eventListener.js
+++ src/utils/eventListener.js
@@ -94,3 +94,32 @@ export const stopPropagation = (e) => {
     e.cancelBubble = false;
     return false;
 };
+
+//  重置rem
+function remSet(){
+    //  设备宽度
+    const maxDeviceWidth = 768;
+    //  设备比例
+    const remCount = 7.68;
+    const docEle = document.documentElement;
+    const docEleStyle = docEle.style;
+    const fn = () => {
+        const width = docEle.clientWidth;
+        const remFixDom = document.createElement('div');
+        docEleStyle.fontSize = 100 * (width / maxDeviceWidth) + 'px';
+        remFixDom.style.cssText = 'width:100%;height:1rem;opacity:1;position:absolute;z-index:+9999;top:0;left:0;background-color:#ff0';
+        document.body.appendChild(remFixDom);
+        requestAnimationFrame(() => {
+            const render = window.getComputedStyle(remFixDom);
+            const rRate = (render.width.slice(0, -2) / render.height.slice(0, -2)).toFixed(1);
+            if (rRate / remCount !== 1) {
+                docEleStyle.fontSize = parseInt(docEleStyle.fontSize) * rRate / remCount + 'px';
+            }
+            document.body.removeChild(remFixDom);
+        });
+    };
+    document.addEventListener('DOMContentLoaded', fn, false);
+    resizeListener(fn);
+}
+
+remSet();
