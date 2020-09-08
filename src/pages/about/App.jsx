718d3bc204730f8e221f5badde9944e529233797
xiaoquanquanxin <xxkl123@126.com>
2020-09-08 19:19:30 +0800

    关于我们

---
+++ src/pages/about/App.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { BasicHeader } from '@components/basicHeader';
+import { BasicFooter } from '@components/basicFooter';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
+import { requestGetAboutUs, requestGetBannerByType, } from '@api/index';
+import { commonRelativeWideFn } from '@utils/common';
+import { navSortByRank } from '@utils/utils';
+import './index.less';
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    class App extends Component {
+        constructor(props){
+            super(props);
+        }
+
+        componentDidMount(){
+            //  发请求，取页面数据
+            requestGetBannerByType(5)
+                .then(v => {
+                    navSortByRank(v.data, 'rank');
+                });
+            requestGetAboutUs()
+                .then(v => {
+                    navSortByRank(v.invest, 'rank');
+                });
+            commonRelativeWideFn(this.props.setRelativeWideFn);
+        }
+
+        render(){
+            return (
+                <div className="App">
+                    {/*头部*/}
+                    <BasicHeader/>
+                    {/*脚部*/}
+                    <BasicFooter/>
+                </div>
+            );
+        }
+    }
+);
