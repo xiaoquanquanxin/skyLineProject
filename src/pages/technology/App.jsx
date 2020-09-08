acb7043f17cec02187e92a58ac70bedc1b6eae1e
xiaoquanquanxin <xxkl123@126.com>
2020-09-08 17:35:57 +0800

    核心技术

--- src/pages/technology/App.jsx
+++ src/pages/technology/App.jsx
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 import './index.less';
 import { BasicHeader } from '@components/basicHeader';
 import { BasicFooter } from '@components/basicFooter';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
+import { requestGetBannerByType } from '@api/index';
+import { commonRelativeWideFn } from '@utils/common';
+import { navSortByRank } from '@utils/utils';
 
-function App(){
-    return (
-        <div className="App">
-            {/*头部*/}
-            <BasicHeader/>
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    class App extends Component {
+        constructor(props){
+            super(props);
+        }
 
-            {/*脚部*/}
-            <BasicFooter/>
-        </div>
-    );
-}
+        componentDidMount(){
+            //  发请求，取页面数据
+            requestGetBannerByType(6)
+                .then(v => {
+                    v.data && v.data.length && navSortByRank(v.data, 'rank');
+                });
+            requestGetBannerByType(7)
+                .then(v => {
+                    v.data && v.data.length && navSortByRank(v.data, 'rank');
+                });
+            commonRelativeWideFn(this.props.setRelativeWideFn);
+        }
 
-export default App;
+        render(){
+            return (
+                <div className="App">
+                    {/*头部*/}
+                    <BasicHeader/>
+                    {/*脚部*/}
+                    <BasicFooter/>
+                </div>
+            );
+        }
+    }
+);
--- src/pages/technology/index.jsx
+++ src/pages/technology/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import '@css/reset.less';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import AppRedux from '@store/reducers';
 import App from './App';
+import '@utils/common';
+import '@css/reset.less';
 
-render(<App/>, document.getElementById('root'));
+let store = createStore(AppRedux);
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
