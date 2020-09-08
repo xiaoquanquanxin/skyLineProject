3d476c1868f3a8b8833125d228dd355de49a7c5e
xiaoquanquanxin <xxkl123@126.com>
2020-09-08 18:36:18 +0800

    智能座舱

---
+++ src/pages/intelligent-cockpit/App.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { BasicHeader } from '@components/basicHeader';
+import { BasicFooter } from '@components/basicFooter';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
+import { requestGetBannerByType, requestGetCockPitPartner } from '@api/index';
+import { commonRelativeWideFn } from '@utils/common';
+import { navSortByRank } from '@utils/utils';
+import './index.less';
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    class App extends Component {
+        constructor(props){
+            super(props);
+        }
+
+        componentDidMount(){
+            //  发请求，取页面数据
+            requestGetBannerByType(16)
+                .then(v => {
+                    navSortByRank(v.data, 'rank');
+                });
+            requestGetCockPitPartner()
+                .then(v => {
+                    navSortByRank(v.data, 'rank');
+                });
+            commonRelativeWideFn(this.props.setRelativeWideFn);
+        }
+
+        render(){
+            return (
+                <div className="App">
+                    {/*头部*/}
+                    <BasicHeader/>
+                    {/*脚部*/}
+                    <BasicFooter/>
+                </div>
+            );
+        }
+    }
+);
