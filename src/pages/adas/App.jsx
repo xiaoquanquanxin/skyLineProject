eb8da42007a1fb3b7442128000469c43fcf84f48
xiaoquanquanxin <xxkl123@126.com>
2020-09-08 18:18:02 +0800

    高级别

---
+++ src/pages/adas/App.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { BasicHeader } from '@components/basicHeader';
+import { BasicFooter } from '@components/basicFooter';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
+import { requestGetBannerByType, requestGetDriverClient } from '@api/index';
+import { commonRelativeWideFn } from '@utils/common';
+import { navSortByRank } from '@utils/utils';
+import './index.less';
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    class App extends Component {
+        constructor(props){
+            super(props);
+        }
+
+        componentDidMount(){
+            //  发请求，取页面数据
+            requestGetBannerByType(13)
+                .then(v => {
+                    navSortByRank(v.data, 'rank');
+                });
+            requestGetBannerByType(20)
+                .then(v => {
+                    navSortByRank(v.data, 'rank');
+                });
+            requestGetDriverClient()
+                .then(v => {
+                    navSortByRank(v.data, 'rank');
+                });
+            commonRelativeWideFn(this.props.setRelativeWideFn);
+        }
+
+        render(){
+            return (
+                <div className="App">
+                    {/*头部*/}
+                    <BasicHeader/>
+                    {/*脚部*/}
+                    <BasicFooter/>
+                </div>
+            );
+        }
+    }
+);
