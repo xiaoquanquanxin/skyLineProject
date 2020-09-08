f2d793ff3e90a0b1cca0f0f4cae8e9edadec0714
xiaoquanquanxin <xxkl123@126.com>
2020-09-08 17:56:56 +0800

    旭日3

---
+++ src/pages/sunrise3/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { BasicHeader } from '@components/basicHeader';
+import { BasicFooter } from '@components/basicFooter';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
+import { requestGetBannerByType } from '@api/index';
+import { commonRelativeWideFn } from '@utils/common';
+import { navSortByRank } from '@utils/utils';
+import './index.less';
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    class App extends Component {
+        constructor(props){
+            super(props);
+        }
+
+        componentDidMount(){
+            //  发请求，取页面数据
+            requestGetBannerByType(10)
+                .then(v => {
+                    v.data && v.data.length && navSortByRank(v.data, 'rank');
+                });
+            commonRelativeWideFn(this.props.setRelativeWideFn);
+        }
+
+        render(){
+            return (
+                <div className="App">
+                    {/*头部*/}
+                    <BasicHeader/>
+                    {/*脚部*/}
+                    <BasicFooter/>
+                </div>
+            );
+        }
+    }
+);
