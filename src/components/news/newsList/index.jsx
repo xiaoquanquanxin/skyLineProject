5322619323a72254efaec284f6278b32d35ec0ce
xiaoquanquanxin <xxkl123@126.com>
2020-09-10 12:00:16 +0800

    新闻页面

--- src/components/news/newsList/index.jsx
+++ src/components/news/newsList/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
-
 import { requestGetNewsList } from '@api/index';
-import { navSortByRank } from '@utils/utils';
+
+import { MainListItem } from '@components/news/newsList/mainListItem';
+import { RelativeArticle } from '@components/news/newsList/relativeArticle';
 import style from './index.module.less';
 
 export const NewsList = connect(
@@ -74,7 +75,7 @@ export const NewsList = connect(
             //  主数据模块
             const mainList = dataList.map(item => {
                 return (
-                    <NewsListItem key={item.id} data={item}/>
+                    <MainListItem key={item.id} data={item}/>
                 );
             });
 
@@ -103,64 +104,3 @@ export const NewsList = connect(
         }
     }
 );
-//  每一项
-const NewsListItem = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(({
-    data,
-    REDUCER_ABOUT_TAB_BOX,
-}) => {
-    const { newsCategoryDataMap } = REDUCER_ABOUT_TAB_BOX;
-    if (newsCategoryDataMap === null) {
-        return '';
-    }
-
-    return (
-        <li key={data.id} className={style.mainListItem}>
-            <div className={style.imgBox}>
-                <img src={data.img} className={style.mainImg} alt={data.span}/>
-                <div className={style.belongType}>
-                    <img src={data.thumb} className={style.belongTypeImg} alt={data.span}/>
-                    <span className={style.span}>{newsCategoryDataMap[data.category_id].name || ''}</span>
-                </div>
-            </div>
-            <div className={style.titleDateDesc}>
-                <p className={style.title}>{data.title}</p>
-                <p className={style.date}>{transformDateType(data.publish_date)}</p>
-                <div className={style.desc}>
-                    {'⚠️这个不对' + matchReg(data.content) + '...'}
-                </div>
-            </div>
-        </li>
-    );
-});
-
-//  右侧列表相关文章
-const RelativeArticle = ({
-    data
-}) => {
-    //  console.log(data);
-    return (
-        <dd className={style.relativeItem}>
-            <div className={style.imgBox}>
-                <img src={data.img} className={style.img} alt=''/>
-            </div>
-            <div className={style.contentBox}>
-                <p className={style.title}>{data.title}</p>
-                <p className={style.date}>{transformDateType(data.publish_date)}</p>
-            </div>
-        </dd>
-    );
-};
-
-//  匹配规则
-function matchReg(str){
-    let reg = /<\/?.+?\/?>/g;
-    return str.replace(reg, '').substr(0, 93);
-}
-
-//  转换时间格式
-function transformDateType(string){
-    return string.replace('-', '年').replace('-', '月') + '日';
-}
