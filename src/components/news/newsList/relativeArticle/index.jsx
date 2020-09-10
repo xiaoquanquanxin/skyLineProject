bc485d287fd864a417baaf39b219efe4026c301a
xiaoquanquanxin <xxkl123@126.com>
2020-09-10 18:53:04 +0800

    换图片地址

--- src/components/news/newsList/relativeArticle/index.jsx
+++ src/components/news/newsList/relativeArticle/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { transformDateType } from '@components/news/common/newsCommon';
+import { transformDateType } from '@utils/utils';
 import style from './index.module.less';
 
 //  右侧列表相关文章
@@ -12,7 +12,7 @@ export const RelativeArticle = ({
         <dd className={style.relativeItem}>
             <div className={style.relativeItemInner}>
                 <div className={style.imgBox}>
-                    <img src={data.img} className={style.img} alt=''/>
+                    <img src={data.thumb} className={style.img} alt=''/>
                 </div>
                 <div className={style.contentBox}>
                     <p className={style.title}>{data.title}</p>
