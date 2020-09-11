85efcccf7651238434704316c4ae1f88bdd7e01e
xiaoquanquanxin <xxkl123@126.com>
2020-09-11 14:39:21 +0800

    header

--- src/components/basicHeader/headerPC/index.jsx
+++ src/components/basicHeader/headerPC/index.jsx
@@ -1,51 +1,57 @@
-import React, { Component } from 'react';
-import CSSModules from 'react-css-modules';
+import React from 'react';
 import { MenuPC } from '@components/basicHeader/menuPC';
 import { GetHeaderLogoMenuInformation, logoClick } from '../common/headerCommon';
 import style from './index.module.less';
 import layout from '@css/layout.module.less';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
 
 //  PC端头部
-export const HeaderPC = CSSModules(({
-    //  页面是处于顶部
-    isTop,
-    //  鼠标浮于上方
-    isOverHeader,
-    //  是否为首页
-    isHomePage,
-    //  数据
-    navListData,
-    //  鼠标浮于header上方
-    headerMouseOver,
-    //  鼠标离开
-    headerMouseLeave,
-}) => {
-//    console.log('是首页',isHomePage);
-    //  渲染信息
-    const headerRenderInfo = GetHeaderLogoMenuInformation(true, isTop, isHomePage, isOverHeader);
-    return (
-        <header className={style.basicHeader}
-                onMouseOver={() => {headerMouseOver();}}
-                onMouseLeave={() => {headerMouseLeave();}}
-        >
-            <div className={`${style.basicHeaderWrap} ${headerRenderInfo.isTopAndHome ? style.isTopAndHome : ''}`}
-                 style={isHomePage ? { position: 'fixed' } : { position: 'relative' }}
+export const HeaderPC = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    ({
+        //  鼠标浮于上方
+        isOverHeader,
+        //  是否为首页
+        isHomePage,
+        //  数据
+        navListData,
+        //  鼠标浮于header上方
+        headerMouseOver,
+        //  鼠标离开
+        headerMouseLeave,
+        //  浏览器滚动信息
+        REDUCER_BROWSER_INFO,
+    }) => {
+        const { scrollTop, scrollLeft } = REDUCER_BROWSER_INFO;
+        //  渲染信息
+        const headerRenderInfo = GetHeaderLogoMenuInformation(true, scrollTop <= 0, isHomePage, isOverHeader);
+        return (
+            <header className={style.basicHeader}
+                    onMouseOver={() => {headerMouseOver();}}
+                    onMouseLeave={() => {headerMouseLeave();}}
             >
-                <div className={`${style.inner} ${layout.clearfix}`}>
-                    {/*logo*/}
-                    <div className={`${style.basicHeaderLogo} ${layout.left}`}>
-                        <img className={`${layout.inlineBlock}`}
-                             src={headerRenderInfo.imageLogoSrc}
-                             onClick={() => (logoClick(isHomePage))}
-                             alt="地平线头部logo"/>
+                <div className={`${style.basicHeaderWrap} ${headerRenderInfo.isTopAndHome ? style.isTopAndHome : ''}`}
+                     style={isHomePage ? { position: 'fixed', left: `${-scrollLeft}px` } : { position: 'relative' }}
+                >
+                    <div className={`${style.inner} ${layout.clearfix}`}>
+                        {/*logo*/}
+                        <div className={`${style.basicHeaderLogo} ${layout.left}`}>
+                            <img className={`${layout.inlineBlock}`}
+                                 src={headerRenderInfo.imageLogoSrc}
+                                 onClick={() => (logoClick(isHomePage))}
+                                 alt="地平线头部logo"/>
+                        </div>
+                        {/*菜单*/}
+                        <MenuPC
+                            navListData={navListData}
+                            isTopAndHome={headerRenderInfo.isTopAndHome}
+                        />
                     </div>
-                    {/*菜单*/}
-                    <MenuPC
-                        navListData={navListData}
-                        isTopAndHome={headerRenderInfo.isTopAndHome}
-                    />
                 </div>
-            </div>
-        </header>
-    );
-}, style, { allowMultiple: true });
+            </header>
+        );
+    }
+);
