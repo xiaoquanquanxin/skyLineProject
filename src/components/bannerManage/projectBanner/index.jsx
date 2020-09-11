fd70d019c92f2b6018969a34e7a3f91d0de8f229
xiaoquanquanxin <xxkl123@126.com>
2020-09-11 19:14:02 +0800

    一类banner

---
+++ src/components/bannerManage/projectBanner/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { mapStateToProps } from '@store/reduxMap';
+import sliderItemStyle from '../sliderItem.module.less';
+import style from './index.module.less';
+import './index.less';
+import { isValidHTTPString, isValidResourceString } from '@utils/utils';
+
+//  轮播每一项
+export const ProjectBannerSliderItem = connect(
+    mapStateToProps
+)(({
+        data,
+        REDUCER_BROWSER_INFO,
+    }) => {
+        const { isRelativeWide } = REDUCER_BROWSER_INFO;
+        //  如果不是合法视频地址
+        if (!isValidHTTPString(data.video) && !isValidResourceString(data.video)) {
+            data.video = null;
+        }
+        console.log(data);
+        return (
+            <div className={`${sliderItemStyle.bannerSlider} ${style.bannerSlider}`}>
+                {
+                    (data.video && isRelativeWide)
+                        ?
+                        <video autoPlay='autoplay' muted='muted' loop='loop' preload='auto' playsInline={true}
+                               webkit-playsinline='true' x5-video-player-type='h5' x5-video-orientation='portraint'
+                               x5-video-player-fullscreen='true'
+                               src={data.video} className={style.sliderItemVideo}/>
+                        : <img className={style.sliderItemImg} src={data.img} alt={data.title || ''}/>
+                }
+                <div className={style.sliderText}>
+                    <p className={style.sliderTitle}>{data.title}</p>
+                    <p className={style.sliderDescription}>{data.desc}</p>
+                </div>
+            </div>
+        );
+    }
+);
