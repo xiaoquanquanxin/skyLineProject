5d7c9cd02c9cadc0a5178110a1b4943f4cbdf38b
xiaoquanquanxin <xxkl123@126.com>
2020-09-11 18:50:48 +0800

    修改banner样式

--- src/components/bannerManage/aboutBanner/index.jsx
+++ src/components/bannerManage/aboutBanner/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from './index.module.less';
+import sliderItemStyle from '../sliderItem.module.less';
 import './index.less';
 //  关于我们，轮播每一项
 export const AboutBannerSliderItem = ({
     data
 }) => {
     return (
-        <div className={style.bannerSlider}>
+        <div className={`${sliderItemStyle.bannerSlider} ${style.bannerSlider}`}>
             <img className={style.bannerImg} src={data.img} alt={data.title || ''}/>
             <p className={style.bannerTitle}>{data.title}</p>
         </div>
