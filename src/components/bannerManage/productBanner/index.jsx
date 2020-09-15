342ad3973f57a6c022020ad190a13129e16c03aa
xiaoquanquanxin <xxkl123@126.com>
2020-09-15 13:57:04 +0800

    c产品轮播

---
+++ src/components/bannerManage/productBanner/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
+import { isValidHTTPString } from '@utils/utils';
+import sliderItemStyle from '@components/bannerManage/sliderItem.module.less';
+import './index.less';
+import style from './index.module.less';
+//  征程、旭日2、旭日3、MATRIX
+export const ProductSlickItem = connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(({
+        data,
+        REDUCER_BROWSER_INFO,
+    }) => {
+        const { isRelativeWide } = REDUCER_BROWSER_INFO;
+        //  如果不是合法视频地址
+        if (!isValidHTTPString(data.video)) {
+            data.video = null;
+        }
+        data.video = null;
+        console.log(data);
+
+        return (
+            <div className={`${sliderItemStyle.bannerSlider} ${style.bannerSlider}`}>
+                {
+                    (data.video && isRelativeWide)
+                        ?
+                        <video autoPlay='autoplay' muted='muted' loop='loop' preload='auto' playsInline={true}
+                               webkit-playsinline='true' x5-video-player-type='h5' x5-video-orientation='portraint'
+                               x5-video-player-fullscreen='true'
+                               src={data.video} className={style.sliderItemVideo}/>
+                        : <div className={style.sliderItemImg} style={{
+                            backgroundImage: `url(${data.img})`
+                        }}/>
+                }
+                <div className={style.textDesc}>
+                    <p className={style.sTitle} dangerouslySetInnerHTML={{ __html: data.title2 }}/>
+                    <p className={style.title} dangerouslySetInnerHTML={{ __html: data.title }}/>
+                    <div className={style.desc} dangerouslySetInnerHTML={{ __html: data.desc }}/>
+                    <div className={style.line}/>
+                </div>
+            </div>
+        );
+    }
+);
