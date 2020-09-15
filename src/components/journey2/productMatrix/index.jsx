4d9009bc45d8e85aed0739b8915e4c3d4e467b3f
xiaoquanquanxin <xxkl123@126.com>
2020-09-15 16:50:36 +0800

    高知觉

---
+++ src/components/journey2/productMatrix/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './index.module.less';
+//  地平线智能驾驶产品矩阵
+export const ProductMatrix = ({
+    data
+}) => {
+    //  console.log(data);
+    return (
+        <div className={style.productMatrix}>
+            <div className={style.productMatrixIn}>
+                <p className={style.title}>{data.title}</p>
+                <div className={style.desc}>{data.desc}</div>
+                <img className={style.img} src={data.img} alt=''/>
+            </div>
+        </div>
+    );
+};
