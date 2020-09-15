50fe58d955a2206e11a017537ceb700fd75dc2c2
xiaoquanquanxin <xxkl123@126.com>
2020-09-15 18:31:08 +0800

    form表单

---
+++ src/components/getMoreBox/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from './index.module.less';
+import getMoreImgSrc from '@media/getMoreBox/bg-contact.png';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
+//  联系我们，获取更多
+export const GetMoreBox = connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(({
+    setPopFormOpenStatus,
+}) => {
+    return (
+        <div className={style.getMoreBox}>
+            <img className={style.getMoreImg} src={getMoreImgSrc} alt=''/>
+            <div className={style.getMoreIn}>
+                <div className={style.getMoreTitle}>联系我们获取更多产品信息</div>
+                <div className={style.inquireBtn} onClick={() => {setPopFormOpenStatus(true);}}>合作咨询</div>
+            </div>
+        </div>
+    );
+});
