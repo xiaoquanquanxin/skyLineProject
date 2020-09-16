616d44c54c1fd8aca50c338f34dbad58d93dd03b
xiaoquanquanxin <xxkl123@126.com>
2020-09-16 17:16:18 +0800

    bpu

--- src/components/technology/bpu/index.jsx
+++ src/components/technology/bpu/index.jsx
@@ -1,55 +1,89 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import style from './index.module.less';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
+import { FRAME_DELAY } from '@utils/constant';
 
-export const Bpu = class extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            //  激活的id
-            activeIndex: null
-        };
-        this.setActive = this.setActive.bind(this);
-    }
+export const Bpu = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    class extends React.Component {
+        listRef;
+        boxRef;
 
-    setActive(activeIndex){
-        this.setState(() => {
-            return {
-                activeIndex,
+        constructor(props){
+            super(props);
+            this.state = {
+                //  激活的id
+                activeIndex: 2,
             };
-        });
-    }
+            this.setActive = this.setActive.bind(this);
+            this.listRef = createRef();
+            this.boxRef = createRef();
+        }
 
-    render(){
-        const { bpuData } = this.props;
-        const { activeIndex } = this.state;
-        let list;
-        if (bpuData && bpuData.list) {
-            list = bpuData.list.map((item, index) => {
-                return (
-                    <BpuItem key={index}
-                             bpuData={item}
-                             activeIndex={activeIndex}
-                             index={index}
-                             setActive={this.setActive}/>
-                );
+        componentDidMount(){
+
+        }
+
+        setActive(activeIndex){
+            this.setState(() => {
+                return {
+                    activeIndex,
+                };
             });
         }
-        return (
-            <div className={style.bpu}>
-                <div className={style.bpuIn}>
-                    <p className={style.title} dangerouslySetInnerHTML={{ __html: bpuData && bpuData.title }}/>
-                    <div className={style.desc} dangerouslySetInnerHTML={{ __html: bpuData && bpuData.desc }}/>
-                    <div className={style.bpuUlBox}>
-                        <ul className={style.list}>
-                            {list}
-                        </ul>
+
+        componentDidUpdate(prevProps, prevState, snapshot){
+            if (this.props.bpuData.list !== (prevProps.bpuData && prevProps.bpuData.list)) {
+                window.requestAnimationFrame(() => {
+                    setTimeout(() => {
+                        //  list数据发生变化
+                        //  console.log(this.boxRef.current);
+                        //  console.log(this.listRef.current);
+                        const boxRef = this.boxRef.current;
+                        const listRef = this.listRef.current;
+                        const diffX = (listRef.offsetWidth - boxRef.offsetWidth) / 2;
+                        //  console.log(listRef.offsetWidth);
+                        //  console.log(boxRef.offsetWidth);
+                        //  console.log(diffX);
+                        boxRef.scrollTo(diffX, 0);
+                    }, FRAME_DELAY * 2);
+                });
+            }
+        }
+
+        render(){
+            const { bpuData } = this.props;
+            const { activeIndex } = this.state;
+            let list;
+            if (bpuData && bpuData.list) {
+                list = bpuData.list.map((item, index) => {
+                    return (
+                        <BpuItem key={index}
+                                 bpuData={item}
+                                 activeIndex={activeIndex}
+                                 index={index}
+                                 setActive={this.setActive}/>
+                    );
+                });
+            }
+            return (
+                <div className={style.bpu}>
+                    <div className={style.bpuIn}>
+                        <p className={style.title} dangerouslySetInnerHTML={{ __html: bpuData && bpuData.title }}/>
+                        <div className={style.desc} dangerouslySetInnerHTML={{ __html: bpuData && bpuData.desc }}/>
+                        <div className={style.bpuUlBox} ref={this.boxRef}>
+                            <ul className={style.list} ref={this.listRef}>{list}</ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
-    }
+            );
+        }
 
-};
+    }
+);
 
 //  每一项
 const BpuItem = ({
