34183ad3b921e7b3fdd23a04074758382a3e6adf
xiaoquanquanxin <xxkl123@126.com>
2020-09-16 18:29:08 +0800

    性能

--- src/components/technology/performance/index.jsx
+++ src/components/technology/performance/index.jsx
@@ -3,43 +3,54 @@ import style from './index.module.less';
 
 import performanceMain from '@media/technology/performanceMain.png';
 //  性能
-export const TechnologyPerformance = ({
-    performanceData
-}) => {
-    let list;
-    if (!performanceData) {
-        return '';
+export const TechnologyPerformance = class extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            //  是否展开
+            isOpen: false
+        };
     }
-//    if (performanceData.list) {
-//        list = performanceData.list.map((item, index) => {
-//            return (
-//                <li className={style.item} key={index}>
-//                    <i className={style.icon}/>
-//                    文字文字文字文字
-//                </li>
-//            );
-//        });
-//    }
-    return (
-        <div className={style.performance}>
-            <div className={style.performanceIn}>
-                <p className={style.title}>{performanceData.title}</p>
-                <div className={style.descBtn}>
-                    <div className={style.desc}>
-                        <span className={style.span}>地平线最新一代 AI 芯片 MAPS（在精度有保障范围内的平均处理速度值）高达 416FPS ，远超主流 AI 芯片。下图为在 ImageNet 图像分类 75 ~  80.5% 精度范围内， MAPS 评估方式下的主流芯片测试结果；右一蓝色折线为地平线最新一代芯片测试结果。</span>
-                        <div className={style.btn}>
-                            {/*//	todo	逻辑*/}
-                            <span className={style.open}>展开</span>
-                            <b className={style.close}>收起</b>
-                            <i className={style.arrow}/>
+
+    //  展开或关闭
+    openOrClose(){
+        const { isOpen } = this.state;
+        this.setState(() => {
+            return {
+                isOpen: !isOpen
+            };
+        });
+    }
+
+    render(){
+        const { performanceData } = this.props;
+        if (!performanceData) {
+            return '';
+        }
+        const { isOpen } = this.state;
+        return (
+            <div className={style.performance}>
+                <div className={style.performanceIn}>
+                    <p className={style.title}>{performanceData.title}</p>
+                    <div className={style.descBtn}>
+                        <div className={style.desc}>
+                             <span className={`${style.content} ${isOpen ? style.active : ''}`}
+                                   dangerouslySetInnerHTML={{ __html: performanceData.content }}/>
+                            <div className={style.btn} onClick={() => {this.openOrClose();}}>
+                                {isOpen
+                                    ? <b className={style.close}>收起</b>
+                                    : <span className={style.open}>展开</span>
+                                }
+                                <i className={style.arrow} style={isOpen ? { transform: 'rotateZ(180deg)' } : {}}/>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={style.chartDescItem}>
-                    <img src={performanceMain} className={style.img}/>
-                {/*    <ul className={style.list}>{list}</ul>*/}
+                    <div className={style.chartDescItem}>
+                        <img src={performanceMain} className={style.img}/>
+                        {/*    <ul className={style.list}>{list}</ul>*/}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
