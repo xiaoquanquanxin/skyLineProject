803d4bc72e1f9b40555f723dd00c6415f71409da
xiaoquanquanxin <xxkl123@126.com>
2020-09-17 19:59:46 +0800

    强大的视频处理能力

---
+++ src/components/matrix/matrixApplyScene/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import style from './index.module.less';
+import layout from '@css/layout.module.less';
+import { Sunrise3basicTitleDesc } from '@components/sunrise3/sunrise3basicTitleDesc';
+
+//  应用场景
+export const MatrixApplyScene = ({
+    applySceneData
+}) => {
+    applySceneData = applySceneData || {};
+    let list;
+    if (applySceneData.list) {
+        list = applySceneData.list.map((item, index) => {
+            return (
+                <MASItem key={index} data={item}/>
+            );
+        });
+    }
+    return (
+        <div className={style.applyScene}>
+            <Sunrise3basicTitleDesc data={applySceneData}
+                                    isLight={true}
+                                    widthType={670}
+                                    descriptionColor={'rgba(0,0,0,0.8)'}
+            />
+            <ul className={style.list}>
+                {list}
+            </ul>
+        </div>
+    );
+};
+//  每一项
+const MASItem = ({
+    data,
+}) => {
+    return (
+        <li className={style.item}>
+            <div className={`${style.imgCenter2} ${layout.imgCenter2}`}
+                 style={{ backgroundImage: `url(${data.img})` }}/>
+            <div className={style.desc} dangerouslySetInnerHTML={{ __html: data.name }}/>
+        </li>
+    );
+};
