0f942d71dbf21d67740aaecb76001bb7acb8bd06
xiaoquanquanxin <xxkl123@126.com>
2020-09-17 17:55:05 +0800

    强大的视频处理能力

---
+++ src/components/matrix/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import style from './index.module.less';
+
+//  荣获多项国际大奖
+export const AwardsBox = ({
+    awardsBoxData
+}) => {
+    awardsBoxData = awardsBoxData || {};
+    let list;
+    if (awardsBoxData.list) {
+        list = awardsBoxData.list.map((item, index) => {
+            return (
+                <AwardsBoxItem key={index} data={item}/>
+            );
+        });
+    }
+    return (
+        <div className={style.awardsBox}>
+            <p className={style.title} dangerouslySetInnerHTML={{ __html: awardsBoxData.title }}/>
+            <ul className={style.awardsList}>
+                {list}
+            </ul>
+        </div>
+    );
+};
+//  每一项
+const AwardsBoxItem = ({
+    data,
+}) => {
+    return (
+        <li className={style.awardsBoxItem}>
+            <div className={style.imgCenter2} style={{ backgroundImage: `url(${data.img})` }}/>
+            <p className={style.name} dangerouslySetInnerHTML={{ __html: data.name }}/>
+            <div className={style.nameDesc} dangerouslySetInnerHTML={{ __html: data.desc }}/>
+        </li>
+    );
+};
