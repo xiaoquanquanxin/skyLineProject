a8a696eae91a06e4cb759d8944d27a180277e3f0
xiaoquanquanxin <xxkl123@126.com>
2020-09-18 17:35:34 +0800

    完成

--- src/components/sunrise3/openExplorer/index.jsx
+++ src/components/sunrise3/openExplorer/index.jsx
@@ -6,12 +6,22 @@ export const OpenExplorer = ({
     openExplorerData,
     //  是浅色的底
     isLight,
+    //  外边距
+    margin
 }) => {
     openExplorerData = openExplorerData || {};
+    let marginClass;
+    switch (margin) {
+        case .2:
+            marginClass = style.marginClassM2;
+            break;
+        default:
+            break;
+    }
     return (
-        <div className={`${style.openExplorer} ${style.isLight}`}>
+        <div className={`${style.openExplorer} ${isLight ? style.isLight : {}}`}>
             <BasicTitleDesc data={openExplorerData} widthType={918} isLight={isLight}/>
-            <img src={openExplorerData.img} className={style.img} alt=''/>
+            <img src={openExplorerData.img} className={`${style.img} ${marginClass}`} alt=''/>
         </div>
     );
 };
