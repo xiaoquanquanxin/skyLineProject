05b7245db619b292b7181a5481a2e8d6f95ce82f
xiaoquanquanxin <xxkl123@126.com>
2020-09-18 17:01:59 +0800

    完成

--- src/components/sunrise3/openExplorer/index.jsx
+++ src/components/sunrise3/openExplorer/index.jsx
@@ -3,13 +3,15 @@ import style from './index.module.less';
 import { BasicTitleDesc } from '@components/basicTitleDesc';
 
 export const OpenExplorer = ({
-    openExplorerData
+    openExplorerData,
+    //  是浅色的底
+    isLight,
 }) => {
     openExplorerData = openExplorerData || {};
     return (
-        <div className={style.openExplorer}>
-            <BasicTitleDesc data={openExplorerData} widthType={918}/>
-            <img src={openExplorerData.img} className={style.img}/>
+        <div className={`${style.openExplorer} ${style.isLight}`}>
+            <BasicTitleDesc data={openExplorerData} widthType={918} isLight={isLight}/>
+            <img src={openExplorerData.img} className={style.img} alt=''/>
         </div>
     );
 };
