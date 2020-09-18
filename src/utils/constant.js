517adf9a4f909f81c679b158cdc4bbb50403a112
xiaoquanquanxin <xxkl123@126.com>
2020-09-18 17:22:07 +0800

    完成

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -11,3 +11,12 @@ export const FRAME_DELAY = 16.667;
 
 //  地图切换动画
 export const BMAP_ANIMATION_BOUNCE = 2;
+
+//  nav_cat_id，getimgtitle接口里用于分模块的id
+export const NAV_CAT_ID = 'nav_cat_id';
+
+//  各个页面请求接口的参数
+//  matrix
+export const MATRIX = { name: 'Matrix', type: 119 };
+//  天工开物
+export const OPEN_EXPLORER = { name: '天工开物' };
