f78fb9f4cbb5fdb97aebf2d5691b10ff9a33ea44
xiaoquanquanxin <xxkl123@126.com>
2020-09-19 10:33:22 +0800

    完成

--- src/components/journey3/ultraLowPower/index.jsx
+++ src/components/journey3/ultraLowPower/index.jsx
@@ -8,15 +8,15 @@ export const UltraLowPower = ({
     contentIsRight,
 }) => {
     ultraLowPowerData = ultraLowPowerData || {};
-//    if (ultraLowPowerData.desc) {
-//        const arr = ultraLowPowerData.desc.split('\t');
-//        console.log(arr);
-//    }
+
+    //  右侧文字多
+    let rightDescMore = contentIsRight && ultraLowPowerData.desc && ultraLowPowerData.desc.length > 100;
     return (
         <div className={style.ultraLowPower}>
             <div className={style.ultraLowPowerInner} style={{ backgroundImage: `url(${ultraLowPowerData.img})` }}>
-                <div className={`${style.contentWrap} ${contentIsRight ? style.contentIsRight : ''}`}>
-                    <p className={style.title}>高性能</p>
+                <div
+                    className={`${style.contentWrap} ${contentIsRight ? style.contentIsRight : ''} ${rightDescMore ? style.rightDescMore : ''}`}>
+                    <p className={style.title} dangerouslySetInnerHTML={{ __html: ultraLowPowerData.title }}/>
                     <ul className={style.list}>
                         <li className={style.item}>
                             <pre className={style.pre}
