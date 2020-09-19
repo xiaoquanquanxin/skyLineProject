a6a2ec779ab9617356b88b1186dedddc477c5d33
xiaoquanquanxin <xxkl123@126.com>
2020-09-19 21:35:54 +0800

    完成

---
+++ src/components/CustomerCase/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import style from './index.module.less';
+import layout from '@css/layout.module.less';
+//  高级别辅助驾驶-客户案例
+export const CustomerCase = ({
+    customerCaseData
+}) => {
+    customerCaseData = customerCaseData || {};
+    console.clear();
+    let list;
+    if (customerCaseData.list) {
+        list = customerCaseData.list.map((item, index) => {
+            return (
+                <CustomerCaseItem key={index} data={item}/>
+            );
+        });
+    }
+    return (
+        <div className={style.customerCase}>
+            <div className={style.customerCaseIn}>
+                <ul className={style.list}>
+                    {list}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+//  每一项
+const CustomerCaseItem = ({ data }) => {
+    console.table(data);
+    return (
+        <li className={style.item}>
+            <div className={`${layout.imgCenter2} ${style.imgCenter2}`}
+                 style={{ backgroundImage: `url(${data.img})` }}/>
+            <div className={style.titleDescAuthor}>
+                <p className={style.title} dangerouslySetInnerHTML={{ __html: data.title }}/>
+                <div className={style.desc} dangerouslySetInnerHTML={{ __html: data.desc }}/>
+                <div className={style.author} dangerouslySetInnerHTML={{ __html: data.title2 }}/>
+            </div>
+        </li>
+    );
+};
