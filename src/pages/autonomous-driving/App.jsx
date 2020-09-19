4c53a69d13f1acf3d8f6663c7272d77eff24bc58
xiaoquanquanxin <xxkl123@126.com>
2020-09-19 21:52:49 +0800

    完成

--- src/pages/autonomous-driving/App.jsx
+++ src/pages/autonomous-driving/App.jsx
@@ -18,6 +18,12 @@ export default connect(
     class App extends Component {
         constructor(props){
             super(props);
+            //  页面宽度监听
+            commonRelativeWideFn(this.props.setRelativeWideFn);
+            //  页面滚动监听
+            getBrowserInfo(this.props.setBrowserScrollInfoFn);
+            const { setBarBoxAnchorList } = props;
+            setBarBoxAnchorList(['优势概述', '合作伙伴',]);
         }
 
         componentDidMount(){
@@ -25,10 +31,7 @@ export default connect(
                 .then(v => {
                     navSortByRank(v.data, 'rank');
                 });
-            //  页面宽度监听
-            commonRelativeWideFn(this.props.setRelativeWideFn);
-            //  页面滚动监听
-            getBrowserInfo(this.props.setBrowserScrollInfoFn);
+
         }
 
         render(){
