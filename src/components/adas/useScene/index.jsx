0ea01117b551e3b9bf3c7c88d7d6a5e2c961e360
xiaoquanquanxin <xxkl123@126.com>
2020-09-19 19:01:23 +0800

    完成

---
+++ src/components/adas/useScene/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import style from './index.module.less';
+import layout from '@css/layout.module.less';
+import { BasicTitleDesc } from '@components/basicTitleDesc';
+
+export const UseScene = ({
+    useSceneData
+}) => {
+    useSceneData = useSceneData || [];
+    const list = useSceneData.map((item, index) => {
+        return (
+            <UseSceneItem key={index} data={item}/>
+        );
+    });
+
+    return (
+        <div className={style.useScene}>
+            <div className={style.useSceneIn}>
+                <BasicTitleDesc data={useSceneData} isLight={true}/>
+                <ul className={style.list}>
+                    {list}
+                </ul>
+            </div>
+        </div>
+    );
+};
+//  每一项
+const UseSceneItem = ({ data }) => {
+    return (
+        <li className={style.item}>
+            <div className={`${layout.imgCenter2} ${style.imgCenter2}`}
+                 style={{ backgroundImage: `url(${data.img})` }}/>
+            <p className={style.name} dangerouslySetInnerHTML={{ __html: data.title }}/>
+            <div className={style.desc} dangerouslySetInnerHTML={{ __html: data.desc }}/>
+        </li>
+    );
+};
