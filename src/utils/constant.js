e598ce06dbf88c1cdec76b20c8e3ab66f291c9e8
xiaoquanquanxin <xxkl123@126.com>
2020-09-20 18:01:08 +0800

    完成

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -26,3 +26,11 @@ export const JOURNEY3 = { name: '征程3', type: 120 };
 export const SUNRISE2 = { name: '旭日2', type: 123 };
 //  智能物联网
 export const AIOT = { name: '智能物联网', type: 129 };
+//  高级别辅助驾驶
+export const ADAS = { name: '高级别辅助驾驶', type: 125 };
+//  自动驾驶
+export const AUTONOMOUS_DRIVING = { name: '自动驾驶', };
+//  智能座舱
+export const INTELLIGENT_COCKPIT = { name: '智能座舱', type: 126 };
+//  高精地图
+export const NAVINET = { name: '高精地图', type1: 127, type2: 128 };
