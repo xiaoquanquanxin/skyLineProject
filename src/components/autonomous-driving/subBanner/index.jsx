91e2a3286c63915ddb6e361294e09194ea77b19c
xiaoquanquanxin <xxkl123@126.com>
2020-09-20 09:28:17 +0800

    完成

---
+++ src/components/autonomous-driving/subBanner/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './index.module.less';
+import layout from '@css/layout.module.less';
+//  二级banner
+export const SubBanner = ({
+    subBannerData
+}) => {
+    subBannerData = subBannerData || {};
+    console.log(subBannerData);
+    return (
+        <div className={style.sbanner}>
+            <div className={style.sbannerIn}>
+                <div className={`${layout.imgCenter2} ${style.imgCenter2}`}
+                     style={{ backgroundImage: `url(${subBannerData.img})` }}
+                />
+                <div className={style.titleDesc}>
+                    <p className={style.title} dangerouslySetInnerHTML={{ __html: subBannerData.title }}/>
+                    <div className={style.desc} dangerouslySetInnerHTML={{ __html: subBannerData.desc }}/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
