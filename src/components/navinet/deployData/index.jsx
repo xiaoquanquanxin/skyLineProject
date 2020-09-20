fbc0db10ee971b69917ec589cf5ab29895f6611a
xiaoquanquanxin <xxkl123@126.com>
2020-09-20 16:32:32 +0800

    完成

---
+++ src/components/navinet/deployData/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './index.module.less';
+
+export const Deploy = ({
+    deployData
+}) => {
+    deployData = deployData || {};
+    return (
+        <div className={style.deploy}>
+            <div className={style.deployIn}
+                 style={{ backgroundImage: `url(${deployData.img})` }}
+            >
+                <p className={style.title} dangerouslySetInnerHTML={{ __html: deployData.title }}/>
+                <div className={style.desc} dangerouslySetInnerHTML={{ __html: deployData.desc }}/>
+            </div>
+        </div>
+    );
+};
