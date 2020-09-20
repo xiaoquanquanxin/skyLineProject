97018249a0f55021aa7b7319d2487239dbd6c93f
xiaoquanquanxin <xxkl123@126.com>
2020-09-20 11:06:55 +0800

    完成

---
+++ src/components/intelligent-cockpit/productArchitecture/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import style from './index.module.less';
+import { BasicTitleDesc } from '@components/basicTitleDesc';
+
+//  产品架构
+export const ProductArchitecture = ({
+    productArchitectureData
+}) => {
+    productArchitectureData = productArchitectureData || {};
+    console.log(productArchitectureData);
+    let list;
+    if (productArchitectureData.list) {
+        const len = productArchitectureData.list.length;
+        list = productArchitectureData.list.map((item, index) => {
+            return (
+                <PAListItem key={index} data={item} index={len - index}/>
+            );
+        });
+    }
+    return (
+        <div className={style.productStruction}>
+            <div className={style.productStrctIn}
+                 style={{ backgroundImage: `url(${productArchitectureData.img})` }}>
+                <BasicTitleDesc data={productArchitectureData} isLight={true}/>
+                <ul className={style.list}>
+                    {list}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const PAListItem = ({ data, index }) => {
+    return (
+        <li className={style.item}>
+            <p className={style.name}>
+                <i className={style.i}>{index}</i>
+                <span className={style.span} dangerouslySetInnerHTML={{ __html: data.title }}/>
+            </p>
+            <div className={style.desc} dangerouslySetInnerHTML={{ __html: data.desc }}/>
+        </li>
+    );
+};
