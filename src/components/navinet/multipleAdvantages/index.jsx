521992fcb11ad7afd8c693a61b575e29c9ee5574
xiaoquanquanxin <xxkl123@126.com>
2020-09-20 18:00:02 +0800

    完成

--- src/components/navinet/multipleAdvantages/index.jsx
+++ src/components/navinet/multipleAdvantages/index.jsx
@@ -3,35 +3,56 @@ import style from './index.module.less';
 import { BasicTitleDesc } from '@components/basicTitleDesc';
 
 //  方案优势
-export const MultipleAdvantages = ({
-    maData
-}) => {
-    maData = maData || {};
-    const { maDataNormal, maDataHover } = maData;
-    if (!maDataNormal) {
-        return '';
+export const MultipleAdvantages = class extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            activeIndex: 2,
+        };
+        this.mouseHover.bind(this);
     }
-    const list = maDataNormal.map((item, index) => {
-        const hoverData = maDataHover[index];
+
+    mouseHover(activeIndex){
+        this.setState(() => {
+            return {
+                activeIndex
+            };
+        });
+    }
+
+    render(){
+        const maData = this.props.maData || {};
+        const { activeIndex } = this.state;
+        const { maDataNormal, maDataHover } = maData;
+        if (!maDataNormal) {
+            return '';
+        }
+        const list = maDataNormal.map((item, index) => {
+            const hoverData = maDataHover[index];
+            return (
+                <li className={`${style.item} ${activeIndex === index ? style.active : ''}`}
+                    onMouseOver={() => {this.mouseHover(index);}}
+                    style={{ backgroundImage: `url(${item.img})` }}
+                    key={index}
+                >
+                    <div className={style.thumb}
+                         style={{ backgroundImage: `url(${hoverData.img})` }}>
+                        <p className={style.name} dangerouslySetInnerHTML={{ __html: item.title }}/>
+                        <div className={style.desc} dangerouslySetInnerHTML={{ __html: hoverData.desc }}/>
+                    </div>
+                </li>
+            );
+        });
         return (
-            <li className={style.item} key={index}
-                style={{ backgroundImage: `url(${item.img})` }}>
-                <div className={style.thumb}
-                     style={{ backgroundImage: `url(${hoverData.img})` }}>
-                    <p className={style.name} dangerouslySetInnerHTML={{ __html: item.title }}/>
-                    <div className={style.desc} dangerouslySetInnerHTML={{ __html: hoverData.desc }}/>
+            <div className={style.multipleAdvantages}>
+                <div className={style.multipleAdvantagesIn}>
+                    <BasicTitleDesc data={{ title: '方案优势' }} isLight={true}/>
+                    <ul className={style.list}>
+                        {list}
+                    </ul>
                 </div>
-            </li>
-        );
-    });
-    return (
-        <div className={style.multipleAdvantages}>
-            <div className={style.multipleAdvantagesIn}>
-                <BasicTitleDesc data={{ title: '方案优势' }} isLight={true}/>
-                <ul className={style.list}>
-                    {list}
-                </ul>
             </div>
-        </div>
-    );
+        );
+    }
+
 };
