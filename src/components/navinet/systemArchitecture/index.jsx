fed6f1d72adc8285893c42eacd07e207e3bfde96
xiaoquanquanxin <xxkl123@126.com>
2020-09-20 15:05:32 +0800

    完成

---
+++ src/components/navinet/systemArchitecture/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './index.module.less';
+import { BasicTitleDesc } from '@components/basicTitleDesc';
+
+//  系统架构
+export const SystemArchitecture = ({
+    systemArchitectureData
+}) => {
+    systemArchitectureData = systemArchitectureData || {};
+    return (
+        <div className={style.sysStructor}>
+            <div className={style.sysStructorIn}
+                 style={{ backgroundImage: `url(${systemArchitectureData.img})` }}>
+                <BasicTitleDesc data={systemArchitectureData} isLight={this}/>
+            </div>
+        </div>
+    );
+};
