e3796be2c9c4cf0910f9be3637dcfc9234df9c87
xiaoquanquanxin <xxkl123@126.com>
2020-09-21 14:34:46 +0800

    完成

--- src/components/technology/performance/index.jsx
+++ src/components/technology/performance/index.jsx
@@ -1,56 +1,69 @@
 import React from 'react';
 import style from './index.module.less';
-
-import performanceMain from '@media/technology/performanceMain.png';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
 //  性能
-export const TechnologyPerformance = class extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            //  是否展开
-            isOpen: false
-        };
-    }
-
-    //  展开或关闭
-    openOrClose(){
-        const { isOpen } = this.state;
-        this.setState(() => {
-            return {
-                isOpen: !isOpen
+export const TechnologyPerformance = connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(class extends React.Component {
+        constructor(props){
+            super(props);
+            this.state = {
+                //  是否展开
+                isOpen: false
             };
-        });
-    }
-
-    render(){
-        const { performanceData } = this.props;
-        if (!performanceData) {
-            return '';
         }
-        const { isOpen } = this.state;
-        return (
-            <div className={style.performance}>
-                <div className={style.performanceIn}>
-                    <p className={style.title}>{performanceData.title}</p>
-                    <div className={style.descBtn}>
-                        <div className={style.desc}>
+
+        //  展开或关闭
+        openOrClose(){
+            const { isOpen } = this.state;
+            this.setState(() => {
+                return {
+                    isOpen: !isOpen
+                };
+            });
+        }
+
+        render(){
+            const performanceData = this.props.performanceData || {};
+            if (!performanceData) {
+                return '';
+            }
+            const { isOpen } = this.state;
+            const { REDUCER_BROWSER_INFO } = this.props;
+            const { isRelativeWide } = REDUCER_BROWSER_INFO;
+            //  切字符串
+            if (performanceData.desc) {
+                performanceData._desc = performanceData.desc.replace('[[[more]]]',
+                    isRelativeWide
+                        ? '<br/>'
+                        : ''
+                );
+            }
+            return (
+                <div className={style.performance}>
+                    <div className={style.performanceIn}>
+                        <p className={style.title}>{performanceData.title}</p>
+                        <div className={style.descBtn}>
+                            <div className={style.desc}>
                              <span className={`${style.content} ${isOpen ? style.active : ''}`}
-                                   dangerouslySetInnerHTML={{ __html: performanceData.content }}/>
-                            <div className={style.btn} onClick={() => {this.openOrClose();}}>
-                                {isOpen
-                                    ? <b className={style.close}>收起</b>
-                                    : <span className={style.open}>展开</span>
-                                }
-                                <i className={style.arrow} style={isOpen ? { transform: 'rotateZ(180deg)' } : {}}/>
+                                   dangerouslySetInnerHTML={{ __html: performanceData._desc }}/>
+                                <div className={style.btn} onClick={() => {this.openOrClose();}}>
+                                    {isOpen
+                                        ? <b className={style.close}>收起</b>
+                                        : <span className={style.open}>展开</span>
+                                    }
+                                    <i className={style.arrow} style={isOpen ? { transform: 'rotateZ(180deg)' } : {}}/>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className={style.chartDescItem}>
-                        <img src={performanceMain} className={style.img}/>
-                        {/*    <ul className={style.list}>{list}</ul>*/}
+                        <div className={style.chartDescItem}>
+                            <img src={performanceData.img} className={style.img} alt=''/>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
-};
+);
