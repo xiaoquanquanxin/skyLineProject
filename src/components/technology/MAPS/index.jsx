96b912b7a4831bbf19af4e6387ec1009908f2816
xiaoquanquanxin <xxkl123@126.com>
2020-09-21 14:42:04 +0800

    完成

--- src/components/technology/MAPS/index.jsx
+++ src/components/technology/MAPS/index.jsx
@@ -1,56 +1,65 @@
 import React from 'react';
 import style from './index.module.less';
-import performanceSub from '@media/technology/performanceSub.png';
+import { splitDesc } from '@utils/utils';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '@store/reduxMap';
 
-export const MAPS = class extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            //  是否展开
-            isOpen: false
-        };
-    }
-
-    //  展开或关闭
-    openOrClose(){
-        const { isOpen } = this.state;
-        this.setState(() => {
-            return {
-                isOpen: !isOpen
+export const MAPS = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(
+    class extends React.Component {
+        constructor(props){
+            super(props);
+            this.state = {
+                //  是否展开
+                isOpen: false
             };
-        });
-    }
-
-    render(){
-        const { mapsData } = this.props;
-        if (!mapsData) {
-            return '';
         }
-        const { isOpen } = this.state;
-        return (
-            <div className={style.assessment}>
-                <div className={style.assessmentIn}>
-                    <div className={style.titleDescBtn}>
-                        <p className={style.title}>关于 MAPS 评估方式</p>
-                        <div className={style.descBtn}>
-                            <div className={style.desc}>
+
+        //  展开或关闭
+        openOrClose(){
+            const { isOpen } = this.state;
+            this.setState(() => {
+                return {
+                    isOpen: !isOpen
+                };
+            });
+        }
+
+        render(){
+            const { mapsData, REDUCER_BROWSER_INFO } = this.props;
+            if (!mapsData) {
+                return '';
+            }
+            const { isOpen } = this.state;
+            mapsData._desc = splitDesc(mapsData.desc, REDUCER_BROWSER_INFO.isRelativeWide);
+            return (
+                <div className={style.assessment}>
+                    <div className={style.assessmentIn}>
+                        <div className={style.titleDescBtn}>
+                            <p className={style.title} dangerouslySetInnerHTML={{ __html: mapsData.title }}/>
+                            <div className={style.descBtn}>
+                                <div className={style.desc}>
                                 <span className={`${style.content} ${isOpen ? style.active : ''}`}
-                                      dangerouslySetInnerHTML={{ __html: mapsData.content }}/>
-                                <div className={style.btn} onClick={() => {this.openOrClose();}}>
-                                    {isOpen
-                                        ? <b className={style.close}>收起</b>
-                                        : <span className={style.open}>展开</span>
-                                    }
-                                    <i className={style.arrow} style={isOpen ? { transform: 'rotateZ(180deg)' } : {}}/>
+                                      dangerouslySetInnerHTML={{ __html: mapsData._desc }}/>
+                                    <div className={style.btn} onClick={() => {this.openOrClose();}}>
+                                        {isOpen
+                                            ? <b className={style.close}>收起</b>
+                                            : <span className={style.open}>展开</span>
+                                        }
+                                        <i className={style.arrow}
+                                           style={isOpen ? { transform: 'rotateZ(180deg)' } : {}}/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className={style.chartDescItem}>
-                        <img src={performanceSub} className={style.img} alt=''/>
+                        <div className={style.chartDescItem}>
+                            <img src={mapsData.img} className={style.img} alt=''/>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
-};
+);
