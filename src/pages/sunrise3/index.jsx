7278921f694d3f9041c423dc0915d3a03823110b
xiaoquanquanxin <xxkl123@126.com>
2020-09-21 11:30:14 +0800

    完成

--- src/pages/sunrise3/index.jsx
+++ src/pages/sunrise3/index.jsx
@@ -1,10 +1,10 @@
+import '@utils/common';
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import AppRedux from '@store/reducers';
 import App from './App';
-import '@utils/common';
 import '@css/reset.less';
 
 let store = createStore(AppRedux);
