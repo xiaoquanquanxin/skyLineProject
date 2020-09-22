6ac285f7f20720d15d31f6bfe193cf9fd2545a1d
xiaoquanquanxin <xxkl123@126.com>
2020-09-22 14:17:00 +0800

    完成

--- src/components/adas/journeyAlgorithm/index.jsx
+++ src/components/adas/journeyAlgorithm/index.jsx
@@ -15,16 +15,14 @@ export const JourneyAlgorithm = connect(
         setVideoOpenStatus,
     }) => {
         jAData = jAData || {};
-        let list = [];
-        if (jAData.content) {
-            //  不是回车、换行
-            const content = jAData.content.split(/(\r|\n|\t|\v)/).filter(item => {
-                return (item !== '\r') && (item !== '\n') && (item !== '\t') && (item !== '\v') && item;
+        const { content } = jAData;
+        let list;
+        if (content) {
+            list = content.map(({ title, desc, }, index) => {
+                return (
+                    <JAItem key={index} data={{ title, desc }}/>
+                );
             });
-            //  console.log(content);
-            for (let i = 0; i < content.length; i += 2) {
-                list.push(<JAItem key={i} data={{ name: content[i], desc: content[i + 1] }}/>);
-            }
         }
         return (
             <div className={style.journeyAlgorithm}>
@@ -49,7 +47,7 @@ export const JourneyAlgorithm = connect(
 const JAItem = ({ data }) => {
     return (
         <li className={style.item}>
-            <p className={style.name} dangerouslySetInnerHTML={{ __html: data.name }}/>
+            <p className={style.name} dangerouslySetInnerHTML={{ __html: data.title }}/>
             <div className={style.desc} dangerouslySetInnerHTML={{ __html: data.desc }}/>
         </li>
     );
