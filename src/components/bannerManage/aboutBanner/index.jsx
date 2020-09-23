9ee8cebacb11049908d5e00a4bb1d962c4f9fc19
xiaoquanquanxin <xxkl123@126.com>
2020-09-23 12:45:31 +0800

    icon

--- src/components/bannerManage/aboutBanner/index.jsx
+++ src/components/bannerManage/aboutBanner/index.jsx
@@ -15,18 +15,19 @@ export const AboutBannerSliderItem = connect(
     }) => {
         const { isRelativeWide } = REDUCER_BROWSER_INFO;
         //  如果不是合法视频地址
+        data._video = data.video;
         if (!isValidHTTPString(data.video) && !isValidResourceString(data.video)) {
-            data.video = null;
+            data._video = null;
         }
         return (
             <div className={`${sliderItemStyle.bannerSlider} ${style.bannerSlider}`}>
                 {
-                    (data.video && isRelativeWide)
+                    (data._video && isRelativeWide)
                         ?
                         <video autoPlay='autoplay' muted='muted' loop='loop' preload='auto' playsInline={true}
                                webkit-playsinline='true' x5-video-player-type='h5' x5-video-orientation='portraint'
                                x5-video-player-fullscreen='true'
-                               src={data.video} className={style.sliderItemVideo}/>
+                               src={data._video} className={style.sliderItemVideo}/>
                         : <img className={style.sliderItemImg} src={data.img} alt={data.title || ''}/>
                 }
                 <p className={style.sliderTitle}>{data.title}</p>
