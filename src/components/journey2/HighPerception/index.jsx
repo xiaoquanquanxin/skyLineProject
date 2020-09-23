5ef8e9ccfdcb5eb8d8a20b760ada13f376642ff5
xiaoquanquanxin <xxkl123@126.com>
2020-09-23 14:22:06 +0800

    icon

--- src/components/journey2/HighPerception/index.jsx
+++ src/components/journey2/HighPerception/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from './index.module.less';
 import { DescList } from '@components/journey2/descList';
+import { setRelativeWide } from '@store/browserInfo';
 
 //  四个一块的
 export const HighPerception = ({
     data,
+    //  宽度适配
+    customWidth,
 }) => {
     data = data || {};
     if (data.content) {
@@ -13,12 +16,24 @@ export const HighPerception = ({
             data.descList.splice(1, 0, null);
         }
     }
+
+    let wrapWidth;
+    switch (customWidth) {
+        case 1:
+            wrapWidth = style.wrapWidth1;
+            break;
+        case 3:
+            wrapWidth = style.wrapWidth3;
+            break;
+        default:
+            break;
+    }
     return (
         <div className={style.highPerception}>
-            <div className={style.highPerceptionIn} style={{ backgroundImage: `url(${data.img || '' })` }}>
-                <div className={style.wrap}>
+            <div className={style.highPerceptionIn} style={{ backgroundImage: `url(${data.img || ''})` }}>
+                <div className={`${style.wrap}`}>
                     <p className={style.title} dangerouslySetInnerHTML={{ __html: data.title }}/>
-                    <div className={style.desc} dangerouslySetInnerHTML={{ __html: data.desc }}/>
+                    <div className={`${style.desc} ${wrapWidth}`} dangerouslySetInnerHTML={{ __html: data.desc }}/>
                     <DescList data={data.descList}/>
                 </div>
             </div>
