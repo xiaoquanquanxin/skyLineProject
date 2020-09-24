48bfeeea9cbf97c7deb9a037df299869533cfdc4
xiaoquanquanxin <xxkl123@126.com>
2020-09-24 18:58:44 +0800

    tag

--- src/components/getMoreBox/index.jsx
+++ src/components/getMoreBox/index.jsx
@@ -9,9 +9,11 @@ export const GetMoreBox = connect(
     mapDispatchToProps,
 )(({
     setPopFormOpenStatus,
+    //  是灰色背景色
+    isGrey,
 }) => {
     return (
-        <div className={style.getMoreBox}>
+        <div className={`${style.getMoreBox} ${isGrey ? style.isGrey : ''}`}>
             <img className={style.getMoreImg} src={getMoreImgSrc} alt=''/>
             <div className={style.getMoreIn}>
                 <div className={style.getMoreTitle}>联系我们获取更多产品信息</div>
