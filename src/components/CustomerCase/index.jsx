e3081cb5acea10961b5ceccdfcbc639f2da25cd2
xiaoquanquanxin <xxkl123@126.com>
2020-09-24 17:25:02 +0800

    icon

--- src/components/CustomerCase/index.jsx
+++ src/components/CustomerCase/index.jsx
@@ -4,7 +4,8 @@ import layout from '@css/layout.module.less';
 import { BasicTitleDesc } from '@components/basicTitleDesc';
 //  高级别辅助驾驶-客户案例
 export const CustomerCase = ({
-    customerCaseData
+    customerCaseData,
+    bgc,
 }) => {
     customerCaseData = customerCaseData || {};
     let list;
@@ -15,8 +16,12 @@ export const CustomerCase = ({
             );
         });
     }
+    const styleObj = {};
+    if (bgc) {
+        styleObj.backgroundColor = bgc;
+    }
     return (
-        <div className={style.customerCase}>
+        <div className={style.customerCase} style={styleObj}>
             <div className={style.customerCaseIn}>
                 <BasicTitleDesc data={customerCaseData} isLight={true}/>
                 <ul className={style.list}>
@@ -32,7 +37,7 @@ const CustomerCaseItem = ({ data }) => {
     return (
         <li className={style.item}>
             <div className={`${layout.imgCenter2} ${style.imgCenter2}`}
-                 style={{ backgroundImage: `url(${data.img || '' })` }}/>
+                 style={{ backgroundImage: `url(${data.img || ''})` }}/>
             <div className={style.titleDescAuthor}>
                 <p className={style.title} dangerouslySetInnerHTML={{ __html: data.title }}/>
                 <div className={style.desc} dangerouslySetInnerHTML={{ __html: data.desc }}/>
