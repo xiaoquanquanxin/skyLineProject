d2bb449db8b5a8e65656604d2069acf3cb2fde7b
xiaoquanquanxin <xxkl123@126.com>
2020-09-25 11:41:34 +0800

    tag

--- config/customWebpackPlugin/titleInject.js
+++ config/customWebpackPlugin/titleInject.js
@@ -117,7 +117,8 @@ const titleInjectFn = (outputName, html) => {
 
             metaSiteName = '征程3｜边缘人工智能芯片全球领导者';
             metaType = 'website';
-            metaUrl = 'http://horizon.wx.h5work.com/journey3.html';
+            metaUrl = 'https://www.horizon.ai/journey3.html';
+
             metaTitle = '征程3｜边缘人工智能芯片全球领导者';
             metaDescription = '地平线是边缘人工智能芯片的全球领导者。得益于前瞻性的软硬结合理念，地平线自主研发兼具极致效能与开放易用性的边缘人工智能芯片及解决方案，可面向智能驾驶以及更广泛的通用 AI 应用领域，提供包括高效能边缘 AI 芯片、丰富算法IP、开放工具链等在内的全面赋能服务。目前，地平线是国内唯一一家实现车规级人工智能芯片量产前装的企业。';
             metaImage = '/upload/202009/22/news713891600754612.png';
