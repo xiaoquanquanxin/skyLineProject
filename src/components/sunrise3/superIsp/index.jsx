b2f6df5dc47e84c24d24f1fd78b27454c74c4e1d
xiaoquanquanxin <xxkl123@126.com>
2020-09-26 00:52:11 +0800

    tag

--- src/components/sunrise3/superIsp/index.jsx
+++ src/components/sunrise3/superIsp/index.jsx
@@ -22,7 +22,7 @@ export const SuperIsp = ({
     return (
         <div className={style.superIsp}>
             <div className={style.superIspIn}>
-                <img className={style.img} src={superIspData.img} alt=''/>
+                <img className={style.img} src={superIspData.img || ''} alt=''/>
                 <div className={style.descText}>
                     <p className={style.title} dangerouslySetInnerHTML={{ __html: superIspData.title }}/>
                     <div className={style.desc} dangerouslySetInnerHTML={{ __html: superIspData.desc }}/>
